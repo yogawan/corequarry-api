d606097b5f9af3849ba1dbf69c13f60784fec0fd
Yogaone <yogawanadityapratama@gmail.com>
2025-11-18 03:20:44 +0700

    feat: implement CRUD operations for warehouse API with GET, PUT, and DELETE methods

---
+++ pages/api/warehouse/[id]/index.ts
@@ -0,0 +1,64 @@
+// @/pages/api/warehouse/[id].ts
+import type { NextApiRequest, NextApiResponse } from "next";
+import Warehouse from "@/models/Warehouse";
+import { mongoConnect } from "@/lib/mongoConnect";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await mongoConnect();
+  const { id } = req.query;
+
+  switch (req.method) {
+    case "GET":
+      try {
+        const gudang = await Warehouse.findById(id);
+        if (!gudang)
+          return res
+            .status(404)
+            .json({ success: false, message: "Warehouse not found" });
+
+        return res.status(200).json({ success: true, data: gudang });
+      } catch (err: any) {
+        return res.status(400).json({ success: false, message: err.message });
+      }
+
+    case "PUT":
+      try {
+        const update = await Warehouse.findByIdAndUpdate(id, req.body, {
+          new: true,
+          runValidators: true,
+        });
+
+        if (!update)
+          return res
+            .status(404)
+            .json({ success: false, message: "Warehouse not found" });
+
+        return res.status(200).json({ success: true, data: update });
+      } catch (err: any) {
+        return res.status(400).json({ success: false, message: err.message });
+      }
+
+    case "DELETE":
+      try {
+        const deleted = await Warehouse.findByIdAndDelete(id);
+
+        if (!deleted)
+          return res
+            .status(404)
+            .json({ success: false, message: "Warehouse not found" });
+
+        return res
+          .status(200)
+          .json({ success: true, message: "Warehouse deleted successfully" });
+      } catch (err: any) {
+        return res.status(400).json({ success: false, message: err.message });
+      }
+
+    default:
+      return res
+        .status(405)
+        .json({ success: false, message: "Method Not Allowed" });
+  }
+}
+
+export default handler;
---
+++ pages/api/warehouse/index.ts
@@ -0,0 +1,33 @@
+// @/pages/api/warehouse/index.ts
+import type { NextApiRequest, NextApiResponse } from "next";
+import Warehouse from "@/models/Warehouse";
+import { mongoConnect } from "@/lib/mongoConnect";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await mongoConnect();
+
+  switch (req.method) {
+    case "GET":
+      try {
+        const gudang = await Warehouse.find();
+        return res.status(200).json({ success: true, data: gudang });
+      } catch (err: any) {
+        return res.status(500).json({ success: false, message: err.message });
+      }
+
+    case "POST":
+      try {
+        const data = await Warehouse.create(req.body);
+        return res.status(201).json({ success: true, data });
+      } catch (err: any) {
+        return res.status(400).json({ success: false, message: err.message });
+      }
+
+    default:
+      return res
+        .status(405)
+        .json({ success: false, message: "Method Not Allowed" });
+  }
+}
+
+export default handler;
